faa64e756e5031197b84e63983aa5f09eda2b952
Gongo <jvilla1714@gmail.com>
2019-05-27 11:44:23 -0400

    Created components for the router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+
+import Home from "./Components/Home Page/Home"
+import About from "./Components/About/About"
+import Contact from "./Components/Contact/Contact"
+import NavBar from "./Components/Nav Bar/NavBar"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    
+    </BrowserRouter>
   );
 }
 
