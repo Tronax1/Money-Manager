61b5738a872472d7a4054ee3cd045766bdcf0080
Gongo <jvilla1714@gmail.com>
2019-05-27 12:17:48 -0400

    Added styling to the Nav bar

--- client/src/Components/Nav Bar/NavBar.js
+++ client/src/Components/Nav Bar/NavBar.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import {NavLink} from "react-router-dom"
+
+import "./NavBar.css"
 
 export default class NavBar extends Component {
     render() {
         return (
-            <div>
+            <div className = "Nav">
+                <div className = "Nav-elements">
+                    <NavLink id = "Nav-Links" to="/">Home</NavLink>
+                    <NavLink id = "Nav-Links" to="/About">About</NavLink>
+                    <NavLink id = "Nav-Links" to="/Contact">Contact</NavLink>
+                </div>
             </div>
         )
     }
