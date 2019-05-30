a696bdbf305abe6c60995c1563c869dade745aa3
Gongo <jvilla1714@gmail.com>
2019-05-30 18:38:21 -0400

    Removed dependencies on donut chart

--- client/src/Components/UserProfile/UserPage.js
+++ client/src/Components/UserProfile/UserPage.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react'
 import "./UserPage.css"
-import DonutChart from 'react-donut-chart';
+
 
 export default class UserPage extends Component {
     render() {
         return (
             <div>            
-                <DonutChart 
-                    data = {[{
-                        label: 'Give you up',
-                        value: 25
-                    },
-                    {
-                        label: ' ',
-                        value: 75,
-                        isEmpty: true
-                    }]} />
+            <p><br/><br/><br/>asdsdfsdf</p>   
             
             </div>
         )
