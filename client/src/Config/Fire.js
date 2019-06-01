6181c36314dcf36c39329b275005d0cbead3353a
Gongo <jvilla1714@gmail.com>
2019-06-01 16:15:37 -0400

    Added encryption files to the API keys

--- client/src/Config/Fire.js
+++ client/src/Config/Fire.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase'
+import configKeys from './APIKeys'
     // Your web app's Firebase configuration
     const firebaseConfig = {
-        apiKey: "AIzaSyAMgYAAOR4l11Q1_YNLd5G3KYS7wXF76LY",
-        authDomain: "moneymanager-fca62.firebaseapp.com",
-        databaseURL: "https://moneymanager-fca62.firebaseio.com",
-        projectId: "moneymanager-fca62",
-        storageBucket: "moneymanager-fca62.appspot.com",
-        messagingSenderId: "1005690093997",
-        appId: "1:1005690093997:web:01f186f4f9eb2627"
+        apiKey: configKeys.apiKey,
+        authDomain: configKeys.authDomain,
+        databaseURL: configKeys.databaseURL,
+        projectId: configKeys.projectId,
+        storageBucket: configKeys.storageBucket,
+        messagingSenderId: configKeys.messagingSenderId,
+        appId: configKeys.appId
   };
   // Initialize Firebase
   const fire = firebase.initializeApp(firebaseConfig);
