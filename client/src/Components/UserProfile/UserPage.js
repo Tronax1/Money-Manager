e6f593c972c4eb3527911818120db97e7b04bb83
Justin Findley <jjfindley@mail.usf.edu>
2019-06-06 21:29:14 -0400

    Graph complete

--- client/src/Components/UserProfile/UserPage.js
+++ client/src/Components/UserProfile/UserPage.js
@@ -4,30 +4,58 @@ import DonutChart from 'react-donut-chart'
 import "./UserPage.css"
 
 export default class UserPage extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            total1:50000,
+            total2:20000
+        } 
+    }
     render() {
         return (
-            <div>            
-              <form className = "Alignment">
+            <div>   
+                <div class = "background">
+
+                
+                 <div class = "Dash">
+                    <h2 class = "Dash-text">Dashboard </h2>
+
+                 </div>
+    
+                    <div class ="donut1"> Income
+                </div>
+
+                <div class = "donut2"> Expenses
+                </div>
+                
+              <div class = "Alignment">
 
               <DonutChart
               data = {[{
-                    label: 'Income',
-                    value: 50000 
-              },
+                    label: '$'+this.state.total1,
+                    value: this.state.total1                        
+              }, 
+
              ]}/>
 
             <DonutChart
               data = {[{
-                    label: 'Expenses',
-                    value: 20000 
+                    label: '$'+this.state.total2,
+                    value: this.state.total2 
               },
+              {
+                label: ' ',
+                value: this.state.total2 * 1.5,
+                isEmpty: true
+                }
                 ]}/>
-
-                </form>
-                <label>
-                        <label className="Text"/>
-                        <h2>Total:</h2>
-                    </label>
+                </div>
+                    
+                        <div class = "Total">
+                        Total: ${this.state.total1 - this.state.total2}
+                    </div>
+                    </div>
+                    
             </div>
         )
     }
